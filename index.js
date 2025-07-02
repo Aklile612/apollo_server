c25fb2b109b6fb370b22da1a1de9b288cd03b67b
Aklile Ansa <aklilemente72@gmail.com>
2025-07-02 18:21:19 +0300

    edited

--- index.js
+++ index.js
@@ -51,11 +51,17 @@ const resolvers={
         },
     },
     Mutation:{
-        
+
         deleteGame(_,args){
             _db.games=_db.games.filter((g)=> g.id !== args.id)
 
             return _db.games
+        },
+        addGame(_,args){
+            let game={
+                ...args.game,
+                id: Math.floor(Math.random()*10000).toString()
+            }
         }
     }
 }
