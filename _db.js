2bd837f64d964fd26e0e8c0d4df1ef0bce2d78fd
Aklile Ansa <aklilemente72@gmail.com>
2025-07-06 21:01:30 +0300

    edited

--- _db.js
+++ _db.js
@@ -16,7 +16,7 @@ let games = [
     {
       id: "1",
       rating: 9,
-      content: "Amazing world and deep lore. ",
+      content: "Amazing world and deep lore. what",
       authorId: "1",
       gameId: "1"
     },
